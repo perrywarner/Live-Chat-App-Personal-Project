31059c9200982cd3217636a96430dcf5f415e77f
Perry Warner <perrywarner11@gmail.com>
2022-06-14 10:08:57 -0400

    add comments to guide the steps to Create a User

--- routes/users.ts
+++ routes/users.ts
@@ -35,6 +35,15 @@ const userData: User[] = [
     },
 ];
 
-export const usersRoute = router.get('/users', function (req, res, next) {
+export const usersRoute = router.get('/users', function (req, res) {
     res.json(userData);
 });
+
+export const createUser = router.put('/users', (req, res) => {
+    // 0: deserialize request
+    // 1: build new User based upon the { name: string } from request body
+    // 2 (filter): since PUT is idempotent, check to see if a user exists with the given "name" from req body
+    // 3a (if filter success): add new User to User store, then:
+    // 3b (if filter success): return response HTTP 201 CREATED with body as the full User (with { id, sentMessages }) that was created
+    // 4 (if filter error): return response HTTP 409 CONFLICT
+})
