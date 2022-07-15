73178f76e3bf6bbe5cc786a75dc1e2dc3a873029
Perry Warner <perrywarner11@gmail.com>
2022-07-15 14:31:52 -0400

    [FE] feat(Redux): add Redux deps & code example

--- client-side/src/components/App/App.tsx
+++ client-side/src/components/App/App.tsx
@@ -1,5 +1,7 @@
 // third party
 import React, { useState } from 'react'
+import { Button } from '@mui/material'
+import { Provider } from 'react-redux'
 
 // intra app
 import { User } from '../../../../models'
@@ -7,11 +9,16 @@ import { MessagePage } from '../../pages/MessagePage'
 import { Login } from '../Login'
 import { AppBar } from '../AppBar'
 
+// intra app - redux example (TODO get rid of this)
+import ReduxDemo from '../../redux-CRA-example/App'
+import store from '../../redux-CRA-example/app/store'
+
 // local
 import './App.css'
 
 export const App = () => {
     const [user, setUser] = useState<User>()
+    const [showRedux, setShowRedux] = useState(false)
 
     const handleLogin = (selectedUser: User) => {
         setUser(selectedUser)
@@ -23,21 +30,39 @@ export const App = () => {
         )
     }
 
-    return (
-        <>
-            <AppBar
-                loggedIn={Boolean(user)}
-                onLoginClick={handleAppbarLoginClick}
-            />
-            <div className="Main-container">
-                {!user ? (
-                    <header className="App-header">
-                        <Login onLogin={handleLogin} />
-                    </header>
-                ) : (
-                    <MessagePage loggedInAs={user} />
-                )}
-            </div>
-        </>
-    )
+    const handleReduxExampleClick = () => {
+        setShowRedux(true)
+    }
+
+    if (showRedux) {
+        return (
+            <Provider store={store}>
+                <ReduxDemo />
+            </Provider>
+        )
+    } else {
+        return (
+            <>
+                <AppBar
+                    loggedIn={Boolean(user)}
+                    onLoginClick={handleAppbarLoginClick}
+                />
+                <div className="Main-container">
+                    {!user ? (
+                        <header className="App-header">
+                            <Login onLogin={handleLogin} />
+                            <Button
+                                onClick={handleReduxExampleClick}
+                                variant="contained"
+                            >
+                                View Redux Example
+                            </Button>
+                        </header>
+                    ) : (
+                        <MessagePage loggedInAs={user} />
+                    )}
+                </div>
+            </>
+        )
+    }
 }
