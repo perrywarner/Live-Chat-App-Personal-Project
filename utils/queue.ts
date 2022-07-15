3df8c14b3ea3d8864ff6ddbbaf08309defaacb28
Perry Warner <perrywarner11@gmail.com>
2022-07-15 11:33:27 -0400

    [BE] chore: run Prettier format

--- utils/queue.ts
+++ utils/queue.ts
@@ -1,60 +1,59 @@
 // Source: ripped off from https://github.com/ThePrimeagen/BunSpreader/blob/master/src/html.ts
 
 type Message = {
-    message: any,
-    time: number,
+    message: any
+    time: number
 }
 
 type QueueNode = {
-    time: Message;
-    next?: QueueNode;
+    time: Message
+    next?: QueueNode
 }
 
 class Queue {
-    private head?: QueueNode;
-    private tail?: QueueNode;
-    public length: number;
+    private head?: QueueNode
+    private tail?: QueueNode
+    public length: number
     constructor() {
-        this.length = 0;
+        this.length = 0
     }
 
     enqueue(time: Message) {
-        this.length++;
-        const node = {time, next: undefined};
+        this.length++
+        const node = { time, next: undefined }
         if (!this.head) {
-            this.head = this.tail = node;
-            return;
+            this.head = this.tail = node
+            return
         }
 
-        this.tail!.next = node;
-        this.tail = node;
+        this.tail!.next = node
+        this.tail = node
     }
     peek(): number | undefined {
         if (!this.head) {
-            return undefined;
+            return undefined
         }
 
-        return this.head.time.time;
+        return this.head.time.time
     }
 
     deque() {
-        this.length--;
+        this.length--
         if (!this.head) {
-            return;
+            return
         }
 
-        const node = this.head;
-        this.head = this.head.next;
-        node.next = undefined;
+        const node = this.head
+        this.head = this.head.next
+        node.next = undefined
     }
 }
 
-const queue = new Queue();
-
+const queue = new Queue()
 
 function empty_queue() {
-    const now = Date.now();
-    while (queue.peek() !== undefined && queue.peek() as number < now) {
-        queue.deque();
+    const now = Date.now()
+    while (queue.peek() !== undefined && (queue.peek() as number) < now) {
+        queue.deque()
     }
 }
