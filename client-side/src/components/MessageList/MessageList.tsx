0178a4059d4a5abe64f0008317038dd415030ac7
Perry Warner <perrywarner11@gmail.com>
2022-08-08 14:36:26 -0400

    [FE] refactor(MessageList): unused code cleanup

--- client-side/src/components/MessageList/MessageList.tsx
+++ client-side/src/components/MessageList/MessageList.tsx
@@ -1,5 +1,5 @@
 // third party
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import List from '@mui/material/List'
 import Skeleton from '@mui/material/Skeleton'
 import { Box, Input, Stack, Typography } from '@mui/material'
@@ -7,23 +7,6 @@ import { Box, Input, Stack, Typography } from '@mui/material'
 // intra-app
 import { MessageListItem } from '../MessageListItem'
 import { useGetMessagesQuery } from '../../api/apiSlice'
-import { Message } from '../../../../models'
-
-const fakeMessages: Message[] = [
-    {
-        data: 'Have a great working week!',
-        sentBy: 'Putri Tanjak',
-        createTime: 0,
-    },
-    {
-        data: 'This new landing page, What do you think?',
-        sentBy: 'Ahmed Medi',
-        createTime: 1,
-    },
-    { data: '(img)', sentBy: 'Ahmed Medi', createTime: 2 },
-    { data: '(Audio)', sentBy: 'Milie Nose', createTime: 3 },
-    { data: 'Ok maszeehh!', sentBy: 'MyFirstUser', createTime: 4 },
-]
 
 interface MessageListProps {
     selectedIndex?: number
@@ -43,57 +26,12 @@ export const MessageList: FC<MessageListProps> = ({
 
     return (
         // TODO replace style "flex: 1" with a <Grid container> and <Grid item xs={4}> combo in the immediate parent of where this <MessageList> gets composed in <MessagePage>
-        //      padding: 0.5rem 1rem 0.5rem 1rem;
         <Stack sx={{ flex: 1 }}>
             <Stack direction={'row'}>
                 <Typography variant="h1">Messages</Typography>
                 <Typography variant="body1">(edit icon)</Typography>
             </Stack>
             <Input placeholder="Search..." />
-            {/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
-            {/* START: TESTBED */}
-            {/* {isError || isLoading || isSuccess ? (
-                <Skeleton>
-                <List sx={{ flex: 'auto' }}>
-                        <Box sx={{ height: '100%' }} />
-                    </List>
-                </Skeleton>
-            ) : (
-                <List sx={{ flex: 'auto' }}>
-                    {fakeMessages.map((message, index) => {
-                        return (
-                            <MessageListItem
-                                message={message}
-                                selected={selectedListIndex === index}
-                                onClick={() => handleListItemClick(index)}
-                                key={index}
-                            />
-                        )
-                    })}
-                    {fakeMessages.map((message, index) => {
-                        return (
-                            <MessageListItem
-                                message={message}
-                                selected={selectedListIndex === index}
-                                onClick={() => handleListItemClick(index)}
-                                key={index}
-                            />
-                        )
-                    })}
-                    {fakeMessages.map((message, index) => {
-                        return (
-                            <MessageListItem
-                                message={message}
-                                selected={selectedListIndex === index}
-                                onClick={() => handleListItemClick(index)}
-                                key={index}
-                            />
-                        )
-                    })}
-                </List>
-            )} */}
-            {/* END: TESTBED */}
-            {/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/}
             {isError ? (
                 <Typography variant="h2">
                     Oops! Failed to load Messages ☹️
@@ -101,7 +39,9 @@ export const MessageList: FC<MessageListProps> = ({
             ) : null}
             {isLoading ? (
                 // <Skeleton>'s dimensions are inferred by its children so child is a 100%H box - read https://mui.com/material-ui/react-skeleton/#inferring-dimensions
-                <Skeleton />
+                <Skeleton>
+                    <Box sx={{ height: '100%' }} />
+                </Skeleton>
             ) : isSuccess && messages ? (
                 // mockup shows "pinned" vs "all" messages, skipping "pinned" for now
                 <List sx={{ flex: 'auto' }}>
