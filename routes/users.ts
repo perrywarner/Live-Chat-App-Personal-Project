55b215d5dcc5af8b52270ae475fd4c4e63b65d8d
perrywarner <perrywarner11@gmail.com>
2024-07-16 15:34:07 -0400

    user route updates

--- routes/users.ts
+++ routes/users.ts
@@ -2,16 +2,18 @@ import { router, User } from '../app'
 import { UserCreateRequest } from '../models'
 
 // TODO get by name
-export const usersRoute = router.get('/users', function (req, res) {
-    res.json(User.getList())
+export const usersRoute = router.get('/users', async function (req, res) {
+    console.log('called user get')
+    res.json(await User.getList())
 })
 
-export const createUser = router.put('/users', (req, res) => {
+export const createUser = router.put('/users', async (req, res) => {
     if (req.body.hasOwnProperty('name')) {
         const submittedUser: UserCreateRequest = { name: req.body.name }
 
         // build new User based upon the { name: string } from request body
-        const createResult = User.create(submittedUser)
+        console.log('called user put')
+        const createResult = await User.create(submittedUser)
 
         if (createResult) {
             return res.status(201).json(createResult)
