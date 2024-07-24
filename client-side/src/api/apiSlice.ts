10dbe13cdae1a3044661294fba5ed436323c5bbc
perrywarner <perrywarner11@gmail.com>
2024-07-24 12:13:34 -0400

    cut some comments in apiSlice.ts

--- client-side/src/api/apiSlice.ts
+++ client-side/src/api/apiSlice.ts
@@ -1,4 +1,3 @@
-// Import the RTK Query methods from the React-specific entry point
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { Message } from '../../../models'
 import { DOMAIN_ROOT, messageUrl } from './constants'
@@ -6,12 +5,8 @@ import { DOMAIN_ROOT, messageUrl } from './constants'
 // Define our single API slice object
 // (docs: https://redux.js.org/tutorials/essentials/part-7-rtk-query-basics#api-slice-parameters)
 export const apiSlice = createApi({
-    // The cache reducer expects to be added at `state.api` (already default - this is optional)
     reducerPath: 'api',
-    // "baseUrl": All of our requests will have URLs starting with 'http://localhost:3001'
-    // "mode: 'no-cors'": note to self - DONT USE THIS ONE!!! Redux / RTK ends up thinking every single request is rejected because of this "opaque response" weirdness: https://stackoverflow.com/questions/36292537/what-is-an-opaque-response-and-what-purpose-does-it-serve
     baseQuery: fetchBaseQuery({ baseUrl: DOMAIN_ROOT }),
-    // The "endpoints" represent operations and requests for this server
     endpoints: (builder) => ({
         getMessages: builder.query<Message[], void>({
             query: () => ({ url: messageUrl, method: 'GET' }),
@@ -26,5 +21,4 @@ export const apiSlice = createApi({
     }),
 })
 
-// Export the auto-generated hook for the `getPosts` query endpoint
 export const { useGetMessagesQuery, useGetMessageQuery } = apiSlice
